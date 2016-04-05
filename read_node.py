f1d647f31ec911bf9f62115410df5a5d076fee9a
David Jagoe <david@jagoe.eu>
2016-04-04 22:29:03 -0700

    Working RPM calculation

--- read_node.py
+++ read_node.py
@@ -35,11 +35,13 @@ class RPMCalculator(object):
     N_DEC_PLACES = 0
     LIMIT_OFF = 3000 # Approx 4.73 mA
     LIMIT_ON = 30000 # Approx 11.36 mA
-    DAMPING_CONSTANT = 2 # seconds
+    ZERO_CONSTANT = 5 # seconds
     
     def __init__(self, gateway_device, register_number):
         self._gateway = gateway_device
         self._register = register_number
+        self._rpm = 0
+        self._state = None
         self._t = 0
 
     def _read_sensor(self):
@@ -54,7 +56,25 @@ class RPMCalculator(object):
             
     def calculate_rpm(self):
         switch_state = self._read_sensor()
-        print switch_state
+        if switch_state is not SWITCHING:
+            if self._state is None:
+                self._t = time.time()
+                self._state = switch_state
+            elif switch_state != self._state:
+                t = time.time()
+                delta = t - self._t
+                self._t = t
+                self._state = switch_state
+                period = 2*delta # in seconds
+                freq = 1.0 / period # in Hz
+                self._rpm = round(freq * 60.0, 0)
+            else:
+                t = time.time()
+                delta = t - self._t
+                if delta > self.ZERO_CONSTANT:
+                    self._t = t
+                    self._rpm = 0
+        return self._rpm
 
 
 def _get_register_number(device_id):
@@ -72,7 +92,7 @@ def _get_register_number(device_id):
 def main_loop(calculator):
     while True:
         time.sleep(0.1)
-        calculator.calculate_rpm()
+        print calculator.calculate_rpm()
 
 
 def main():
